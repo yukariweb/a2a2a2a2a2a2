fc89abdd60466272ce118f98f0f9ed522cea0526
manaka-web <enyukari.a.manaka@gmail.com>
2026-01-20 13:28:40 +0900

    22aa22

---
+++ data.js
@@ -0,0 +1,473 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "公園出入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.425212434056977,
+        "pitch": 0.093976243913799,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.308676539158828,
+          "pitch": 0.145491668838865,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6719656486762648,
+          "pitch": 0.19194129644744606,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">ベンチ</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.8567990791484608,
+          "pitch": -0.013145611429491666,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">ブランコ</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.0774148463514326,
+          "pitch": 0.23564852567948869,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">砂場</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.5607277258214696,
+          "pitch": 0.00393551597042574,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">滑り台</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.4050544216018288,
+          "pitch": -0.16471407840155905,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">コンクリートの壁</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.003716430793114,
+          "pitch": -0.21060752423347218,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">民家</font></font>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "ブランコ横",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.4945749794016034,
+        "pitch": 0.04524892310941553,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0745114999529237,
+          "pitch": 0.17935790084595027,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.19009895020129974,
+          "pitch": 0.18827453147902062,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8443706208137023,
+          "pitch": -0.11096663143933228,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">滑り台</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.157801118365697,
+          "pitch": 0.11170322506935548,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">ベンチ</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.9740557731221102,
+          "pitch": 0.1945480890689435,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">砂場</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.32115435327359165,
+          "pitch": 0.09989518725766366,
+          "title": "<div>ベンチ</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.397312364364117,
+          "pitch": 0.03195950201640407,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">ブランコ</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.804066955648559,
+          "pitch": 0.03903101820201016,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">鉄棒前</font></font>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "鉄棒前",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.8464433465546577,
+        "pitch": 0.06059321655931171,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.917155023514109,
+          "pitch": 0.13669148523648822,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.04233160153808058,
+          "pitch": 0.11666128165313872,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5345811801148255,
+          "pitch": 0.056297078427082425,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">滑り台</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.6090770043609126,
+          "pitch": 0.4238675502952649,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">鉄棒</font></font>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "滑り台乗り場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.4111895628642941,
+        "pitch": 0.058280653123979675,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6737748397295888,
+          "pitch": 0.17670643432348854,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.13793773373124196,
+          "pitch": 0.11706832604195405,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8354806702887636,
+          "pitch": 0.07838066192701021,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">鉄棒</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.504727424229868,
+          "pitch": 0.11608924364925066,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">滑り台</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.454554527300136,
+          "pitch": -0.4568771338075628,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">民家</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.3804375516431939,
+          "pitch": 0.09486866856739162,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">ベンチ</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.3866621232522114,
+          "pitch": -0.18268272347840764,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">コンクリート壁</font></font>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "砂場前",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.7521498124489359,
+        "pitch": 0.017360249047248644,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7524070546122168,
+          "pitch": 0.12977912018765636,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 2.7862031369969387,
+          "pitch": 0.14065062890569102,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6399307390196576,
+          "pitch": 0.05487200759085731,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">ベンチ</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.3054705121973882,
+          "pitch": -0.059129916248577885,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">ブランコ</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.4130702523432586,
+          "pitch": 0.036997949020994625,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">滑り台</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.9561657662395104,
+          "pitch": -0.1245769808163697,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">コンクリートの壁</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.960485218612078,
+          "pitch": -0.34174218679168433,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">民家</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.0006134031447331267,
+          "pitch": 0.3542960417981291,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">砂場</font></font>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "ベンチ前",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.9473545362029796,
+        "pitch": 0.10980033243594534,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.062473441498698,
+          "pitch": 0.4033657277877545,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.341173736163327,
+          "pitch": 0.373505871645067,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.294052784775918,
+          "pitch": 0.29792542633230745,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">ベンチ</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.141672915953059,
+          "pitch": 0.06119210666272856,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">ブランコ</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.0372602581389803,
+          "pitch": -0.18947147742437842,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">民家</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.3599030468510165,
+          "pitch": -0.19168857996767485,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">コンクリートの壁</font></font>",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
